e9ad933273e303fc053fdcf24b8f1bd4f641e633
enslow <ryan.enslow.spam@gmail.com>
2023-09-04 21:54:55 -0700

    -added popovers for previous wins

--- client/src/components/PreviousMaps.js
+++ client/src/components/PreviousMaps.js
@@ -1,29 +1,34 @@
+import { map } from 'mathjs'
 import React, { useState, useEffect } from 'react'
-import { ListGroup, Row, Col } from 'react-bootstrap'
+import { ListGroup, Row, Col, OverlayTrigger, Popover } from 'react-bootstrap'
 
-
-//TODO: Add a popover for the thumbnail that contains the map name
-
-// Takes the MOTD as well as the corresponds cookie in the props
 function MapLinkFromLocal(props) {
 
   const saveData = JSON.parse(localStorage.getItem('DailyMap'.concat(props.MOTD)))
+
+  const mapPreview = (
+    <Popover id='popover-basic'>
+      <Popover.Body>
+        <a href={props.link} target='_blank'><h4>{props.title}</h4></a>
+        
+      </Popover.Body>
+    </Popover>)
 
   const scores = (guesses, won) => {
 
     let scoreList = []
 
     for (var i = 0; i < guesses; i++) {
-      scoreList.push(<div className='bg-danger rounded-2 d-inline px-2 m-1'></div>)
+      scoreList.push(<div className='bg-danger rounded-2 d-inline px-3 py-2 m-1'></div>)
     }
 
     if (won) {
       scoreList.pop()
-      scoreList.push(<div className='bg-success rounded-2 d-inline px-2 m-1'></div>)
+      scoreList.push(<div className='bg-success rounded-2 d-inline px-3 py-2 m-1'></div>)
     }
 
     while (scoreList.length < 6) {
-      scoreList.push(<div className='bg-secondary rounded-2 d-inline px-2 m-1'></div>)
+      scoreList.push(<div className='bg-secondary rounded-2 d-inline px-3 py-2 m-1'></div>)
     }
 
     return(
@@ -32,24 +37,24 @@ function MapLinkFromLocal(props) {
   }
 
   var col2 = scores(0, false)
-  var col3 = <p>Not Played!</p>
+  var col3 = <>Not Played!</>
 
   if (saveData !== null) {
     col2 = scores(saveData.guesses.length,saveData.won)
-    col3 = saveData.won !== null ? <img className='img-thumbnail' src={props.mapBG}></img> : <p>Not Finished!</p>
+    col3 = saveData.won !== null ? <OverlayTrigger placement='right' delay={{ show: 100, hide: 400 }} overlay={mapPreview}><img className='img-thumbnail' src={props.mapBG}></img></OverlayTrigger> : <>Not Finished!</>
   }
 
   return(
     <a className='text-decoration-none text-center' href={"previous-maps/"+props.MOTD}>
     <ListGroup.Item action>
-      <Row className='justify-content-md-center align-items-center'>
-        <Col>
+      <Row className='justify-content-md-center align-items-center' style={{height: '70px'}}>
+        <Col className='fs-3'>
           Map Number {props.MOTD}
         </Col>
         <Col>
           {col2}
         </Col>
-        <Col>
+        <Col className='fs-3 align-items-center'>
           {col3}
         </Col>
       </Row>
@@ -67,11 +72,11 @@ export default function PreviousMaps(props) {
 
 
   //let dayList = backendData.map((mapinfo, index) => {return <MapLink MOTD={mapinfo.MOTD}/>})
-  let dayList = backendData.map((mapinfo, index) => {return <MapLinkFromLocal key={index} MOTD={mapinfo.MOTD} mapBG={mapinfo.background}/>})
+  let dayList = backendData.map((mapinfo, index) => {return <MapLinkFromLocal key={index} MOTD={mapinfo.MOTD} mapBG={mapinfo.background} title={mapinfo.title} link={mapinfo.map_url}/>})
 
   return (
     <div className='row justify-content-md-center'>
-      <div className='col-5 list-group'>
+      <div className='col-7 list-group'>
         {dayList}
       </div>
       <div style={{height:'50px'}}></div>
