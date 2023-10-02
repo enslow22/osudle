02281c19567f1c3769feb9bb1ed4b2b7c5f03998
enslow <ryan.enslow.spam@gmail.com>
2023-10-01 23:57:33 -0700

    Added a cache (Hope this works!)

--- server/server.js
+++ server/server.js
@@ -1,14 +1,19 @@
 const express = require('express')
+const apicache = require('apicache')
 const cors = require('cors')
 const app = express()
+const cache = apicache.middleware
 const mysql = require('mysql2')
 const dayjs = require('dayjs')
+
 require('dotenv').config()
 var timezone = require('dayjs/plugin/timezone')
 dayjs.extend(timezone)
 dayjs.tz.setDefault("America/Los_Angeles")
+
 app.use(express.json())
 app.use(cors())
+app.use(cache('5 minutes'))
 
 const db = mysql.createPool({
 	host: process.env.REACT_APP_HOST,
@@ -18,14 +23,6 @@ const db = mysql.createPool({
     port: process.env.REACT_APP_PORT
 });
 
-app.get("/api", (req, res) => {
-    const q = "SELECT * FROM osumapinfo"
-    db.query(q, (err, data) => {
-        if (err) return res.json(err)
-        return res.json(data)
-    })
-})
-
 app.get("/api/dailies", (req, res) => {
     const elapsed = parseInt(dayjs().diff(dayjs('2023-09-21 19:27'), 'day', true))
     const q = `SELECT * FROM osumapinfo WHERE MOTD != -1 AND MOTD <= ${elapsed}`
@@ -33,7 +30,7 @@ app.get("/api/dailies", (req, res) => {
         if (err) return res.json(err)
         return res.json(data)
     })
-})
+});
 
 app.get("/api/titles", (req, res) =>{
     const q = "SELECT title, diff_name FROM osumapinfo"
@@ -41,7 +38,7 @@ app.get("/api/titles", (req, res) =>{
         if (err) return res.json(err)
         return res.json(data)
     })
-})
+});
 
 app.get("/devapi", (req, res) => {
     const q = "SELECT * FROM devtable"
@@ -49,6 +46,6 @@ app.get("/devapi", (req, res) => {
         if (err) return res.json(err)
         return res.json(data)
     })
-})
+});
 
-app.listen(5000, () => {console.log("Server started on port 5000")})
+app.listen(5000, () => {console.log("Server started on port 5000")});
